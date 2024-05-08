496ec89370bdfe2e8a101fa6102ff4701d227ee1
Dapo <odapo138@gmail.com>
2024-05-08 23:28:46 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int num1, num2;
+
+
+Console.WriteLine("Hello, welcome to the calculator program!");
+
+Console.Write("Please enter your first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter your second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("What type of operation would you like to do?");
+Console.Write("Please enter a for addition, s for subtraction, m for multiplication or d for division");
+
+
+Console.ReadLine();
