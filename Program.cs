28aec0012d3a837686430cfa5d74d10bbdadc1b2
Dapo <odapo138@gmail.com>
2024-05-08 23:47:00 +0100

    feat: completed calculator

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿int num1, num2;
+string operation;
 
+int result;
 
 Console.WriteLine("Hello, welcome to the calculator program!");
 
@@ -10,7 +12,28 @@ Console.Write("Please enter your second number: ");
 num2 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("What type of operation would you like to do?");
-Console.Write("Please enter a for addition, s for subtraction, m for multiplication or d for division");
+Console.Write("Please enter a for addition, s for subtraction, m for multiplication or d for division: ");
 
+operation = Console.ReadLine()!;
+
+if (operation == "a")
+{
+    result = num1 + num2;
+}
+else if (operation == "s")
+{
+    result = num1 - num2;
+}
+else if (operation == "m")
+{
+    result = num1 * num2;
+}
+else
+{
+    result = num1 / num2;
+}
+
+Console.WriteLine("The result is " + result);
+Console.WriteLine("Thank you for using the calculator program!");
 
 Console.ReadLine();
